b7d94f903785a0baf35706a7b5bf2ec421c13ff0
Harshit Gupta <hgupta07@in.ibm.com>
2020-07-12 15:48:00 +0530

    Plugin changes

--- request/api.go
+++ request/api.go
@@ -1,11 +1,18 @@
-package request
+package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
 )
+
+type Details struct {
+	Name   interface{}
+	Adress interface{}
+	Image  string
+}
 
 func GetGeocodeLocation(s string) (float64, float64) {
 	google_api_key := "AIzaSyAcKRO63SCy87HvebSXO0v6Gjp444PwNG8"
@@ -36,4 +43,74 @@ func GetGeocodeLocation(s string) (float64, float64) {
 		}
 	}
 	return latitude, longitude
+}
+
+func FindARestaurant(mealType string, location string) []Details {
+
+	var foursquare_client_id = "PQSPJYQ4ODOORBDB51EQTURIFOQT0PACPFQI2UN0G0P00DAF"
+	var foursquare_client_secret = "3ZUE0PGPDY2KV4UFPWEQGLZ4GNDWC2PZHFTX40CKZTCIA3LP"
+
+	latitude, longitude := GetGeocodeLocation(location)
+	lat := fmt.Sprintf("%f", latitude)
+	long := fmt.Sprintf("%f", longitude)
+	var AppendData []Details
+	var restaurantInfo Details
+
+	url := "https://api.foursquare.com/v2/venues/search?client_id=" + foursquare_client_id + "&client_secret=" + foursquare_client_secret + "&v=20130815&ll=" + lat + "," + long + "&query=" + mealType
+
+	resp, err := http.Get(url)
+	if err != nil {
+		panic("Error")
+	}
+	body, err2 := ioutil.ReadAll(resp.Body)
+	var data map[string]interface{}
+	json.Unmarshal(body, &data)
+	if err2 != nil {
+		panic("Error")
+	}
+	for _, v := range data["response"].(map[string]interface{}) {
+
+		for _, v2 := range v.([]interface{}) {
+			id := v2.(map[string]interface{})["id"].(string)
+			rest_name := v2.(map[string]interface{})["name"]
+			rest_address := v2.(map[string]interface{})["location"].(map[string]interface{})["formattedAddress"]
+			Imgurl := "https://api.foursquare.com/v2/venues/" + id + "/photos?client_id=" + foursquare_client_id + "&v=20150603&client_secret=" + foursquare_client_secret
+			imageResp, err3 := http.Get(Imgurl)
+			RespBody, err4 := ioutil.ReadAll(imageResp.Body)
+			if err3 != nil || err4 != nil {
+				panic(err3)
+			}
+			var ImgData map[string]interface{}
+			json.Unmarshal(RespBody, &ImgData)
+
+			var prefix string
+			var suffix string
+			var imageURL string
+
+			for _, v := range ImgData["response"].(map[string]interface{}) {
+				items := v.(map[string]interface{})["items"]
+				for _, v := range items.([]interface{}) {
+					prefix = v.(map[string]interface{})["prefix"].(string)
+					suffix = v.(map[string]interface{})["suffix"].(string)
+
+					if prefix != "" && suffix != "" {
+						imageURL = prefix + "300x300" + suffix
+					}
+
+				}
+			}
+
+			if imageURL == "" {
+				imageURL = "https://cdn.pixabay.com/photo/2014/12/21/23/36/hamburgers-575655_1280.png"
+			}
+
+			restaurantInfo = Details{rest_name, rest_address, imageURL}
+
+			AppendData = append(AppendData, restaurantInfo)
+
+		}
+
+	}
+
+	return AppendData
 }
